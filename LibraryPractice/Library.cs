4dd959da701e590779e5e073ba4c56645e50ccf3
auro <auro@DESKTOP-AV8I9BT>
2024-08-13 16:50:17 +0300

    Add project files.

---
+++ LibraryPractice/Library.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryPractice
+{
+    public class Library
+    {
+
+        //Burada default constructor parametre almadan çalıştırılan kod yazıyoruz.
+        public string _bookName;
+        public int _bookPageNum;
+        public string _name;
+        public string _surname;
+        public string _publisher;
+        public DateTime _registrationDate;
+
+        public string BookName
+        {
+            get
+            {
+                return _bookName;
+            }
+            set
+            {
+                _bookName = value;
+            }
+        }
+        public int BookPageNum
+        {
+            get
+            {
+                return _bookPageNum;
+            }
+            set
+            {
+                _bookPageNum = value;
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+        public string Surname
+        {
+            get
+            {
+                return _surname;
+            }
+            set
+            {
+                _surname = value;
+            }
+        }
+        public string Publisher
+        {
+            get
+            {
+                return _publisher;
+            }
+            set
+            {
+                _publisher = value;
+            }
+        }
+        public DateTime RegistrationDate
+        {
+            get
+            {
+                return _registrationDate;
+            }
+            set
+            {
+                _registrationDate = value;
+            }
+        }
+
+        // Burada ise parametre vererek oluşturulan bir constructor oluşturuyoruz.
+
+        public Library(string bookName, int bookPageNum, string name, string surname, string publisher)
+        {
+            _bookName = bookName;
+            _bookPageNum = bookPageNum;
+            _name = name;
+            _surname = surname;
+            _publisher = publisher;
+        }
+        public void Conclusion()
+        {
+            _registrationDate = DateTime.Now;
+            Console.WriteLine($"{_name} {_surname}'in {_bookPageNum} sayfalık, {_publisher} yayınevinden çıkan {_bookName} kitabıdır.\nKayıt Tarihi: {_registrationDate}");
+        }
+    }
+    
+}
+
+
+
+
+
+
+
+
+   
+
+
+
---
+++ LibraryPractice/Program.cs
@@ -0,0 +1,24 @@
+﻿using LibraryPractice;
+// Default constructor parametre almadan çalıştırılan kod
+Library book1 = new Library();
+book1.BookName = "Adı Aylin";
+book1.BookPageNum = 398;
+book1.Name = "Ayşe";
+book1.Surname = "Kulin";
+book1.Publisher = "Remzi Kitapevi";
+book1.Conclusion();
+
+// Parametre alarak oluşturulan constructor.
+
+Library book2 = new Library("Adı Aylin", 398, "Ayşe", "Kulin", "Remzi Kitapevi");
+book2.Conclusion();
+
+// --------------------Bilgilendirme----------------------
+
+// Class = Nesne oluşturma şablonudur.
+
+// Property = Fieldların erişimlerini kontrol etmek için kullanılır ve get, set erişim metotları ile tanımlanır.
+
+// New = Bir sınıftan yeni bir nesne oluşturur.
+
+// Constructor = Yapıcı metotlardır. Bir sınıfın nesnesi oluşturulduğunda çalışan metottur. Sınıfın başlangıç durumunu ayarlamak için kullanılır.
